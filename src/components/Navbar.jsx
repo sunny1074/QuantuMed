710cb266b37b823dba445c81a185e19dadff9606
Sunny kumar <palsunnykumar1074@gmail.com>
2026-03-29 23:55:00 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Navbar.css';
 
-const navbar = () => {
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <nav className="navbar">
+      <div className="nav-logo">HealthCare</div>
+      
+      {/* Links - conditional class for mobile menu */}
+      <ul className={`nav-links ${isOpen ? "open" : ""}`}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
 
-export default navbar
+      {/* Hamburger Icon for Mobile */}
+      <div className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
